d4bbedb300d2cb4111d423f0824c1bdb5c22a41a
ESHA AGARWAL <80843306+eshaagarwa@users.noreply.github.com>
2024-06-17 00:18:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -128,8 +128,8 @@
                     <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                         <div class="p-3" style="max-width: 900px;">
                             <h5 class="text-white text-uppercase mb-3 animated slideInDown">Creative & Innovative</h5>
-                            <h2 class="display-1 text-white mb-md-4 animated zoomIn">
-                                Empowering Businesses with Emerging Technologies</h2>
+                            <h1 class="display-1 text-white mb-md-4 animated zoomIn">
+                                Empowering Businesses with Emerging Technologies</h1>
                             <a href="service.php" class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">Learn More</a>
                             <a href="contact.php" class="btn btn-outline-light py-md-3 px-md-5 animated slideInRight">Contact
                                 Us</a>
